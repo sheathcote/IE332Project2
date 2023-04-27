400caabddd97b0b024cce79446fb9505131dc5b5
sheathcote <sheathco@purdue.edu>
2023-04-27 16:21:55 -0400

    Update MainAlgorithm.R
    First attempt at actual code for main algorithm ideas

--- MainAlgorithm.R
+++ MainAlgorithm.R
@@ -1,36 +1,85 @@
 
 #majority voting classifier that puts the other five algorithms together
-#img is the image to be modified
+#img is the image to be modified stored as an array
 #b is the pixel budget
-main <- function(img, b) {
+#imgtype is 1 for dandelion and 2 for grass
+main <- function(img, b, imgtype = 1) {
   
-  #load the image as an array
+  #get image type from folder name
+  if (imgtype == 1) {
+    target_type <- 2
+  } else {
+    target_type <- 1
+  }
+
   
-  #get image type from foldername
-  imgtype <- "dandelion"
-  
-  #call 5 sub algorithms and save resulting images
-  img1 <- generate_jsma(model, img, imgtype, epochs = b)
+  #call 5 sub algorithms and save resulting image matrices
+  img1 <- generate_jsma(model, img, target_type, epochs = b)
   img2 <- FGSMFunction(img, b)
   img3 <- PGDFunction(img, b)
   img4 <- SVMFunction(img, b)
   img5 <- LastFunction(img, b)
   
-  #compare image to original image and make matrix of 1's and 0's
+  #compare image to original image matrix and make matrix of 1's and 0's
   #where 1's are changed pixels, 0's are unmodified pixels
-  
+  x_size <- length(img[,1,1])
+  y_size <- length(img[1,,1])
+  matrix1 <- matrix(data = 0, nrow = x_size, ncol = y_size)
+  matrix2 <- matrix(data = 0, nrow = x_size, ncol = y_size)
+  matrix3 <- matrix(data = 0, nrow = x_size, ncol = y_size)
+  matrix4 <- matrix(data = 0, nrow = x_size, ncol = y_size)
+  matrix5 <- matrix(data = 0, nrow = x_size, ncol = y_size)
+  #loop through image
+  for (i in 1:x_size) {
+    for (j in 1:y_size) {
+      if ((abs(img1[i,j,1] - img[i,j,1]) > 0.05) || (abs(img1[i,j,2] - img[i,j,2]) > 0.05) 
+          ||(abs(img1[i,j,3] - img[i,j,3]) > 0.05)) {
+        matrix1[i,j] <- 1
+      }
+      if ((abs(img2[i,j,1] - img[i,j,1]) > 0.05) || (abs(img2[i,j,2] - img[i,j,2]) > 0.05) 
+          ||(abs(img2[i,j,3] - img[i,j,3]) > 0.05)) {
+        matrix2[i,j] <- 1
+      }
+      if ((abs(img3[i,j,1] - img[i,j,1]) > 0.05) || (abs(img3[i,j,2] - img[i,j,2]) > 0.05) 
+          ||(abs(img3[i,j,3] - img[i,j,3]) > 0.05)) {
+        matrix3[i,j] <- 1
+      }
+      if ((abs(img4[i,j,1] - img[i,j,1]) > 0.05) || (abs(img4[i,j,2] - img[i,j,2]) > 0.05) 
+          ||(abs(img4[i,j,3] - img[i,j,3]) > 0.05)) {
+        matrix4[i,j] <- 1
+      }
+      if ((abs(img5[i,j,1] - img[i,j,1]) > 0.05) || (abs(img5[i,j,2] - img[i,j,2]) > 0.05) 
+          ||(abs(img5[i,j,3] - img[i,j,3]) > 0.05)) {
+        matrix5[i,j] <- 1
+      }
+    }
+  }
   
   #get random weights
+  weights <- matrix(runif(5,0,1), ncol=5)
+  weights <- weights/sum(weights)
   
   
   #multiply the matrices by the weights
-  
+  matrix1 <- weights[1] * matrix1
+  matrix2 <- weights[2] * matrix2
+  matrix3 <- weights[3] * matrix3
+  matrix4 <- weights[4] * matrix4
+  matrix5 <- weights[5] * matrix5
   
   #sum all the matrices
+  sumMatrix <- sum(matrix1, matrix2, matrix3, matrix4, matrix5)
   
   
   #find the location of the max value in the matrix 
   #and change the corresponding pixel in the image
+  for (i in 1:b) {
+    index <- which(sumMatrix == max(sumMatrix), arr.ind = TRUE)
+    #find which matrix had the max value and set the img pixel value to the 
+    #pixel values from the corresponding image
+    img[index[1],index[2],] <- img[index[1],index[2],]
+  }
   
-  
+  #return the image
+  return(img)
 }
